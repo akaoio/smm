f729df99246b0d67906a0f508f267d0b32345a9f
MIMIZA <dev@mimiza.com>
2023-11-26 17:57:06 +0800

    add new fields and funtions: profile, refresh

--- smm/libs/telegrambot.py
+++ smm/libs/telegrambot.py
@@ -33,17 +33,32 @@ def profile(**args):
     agent = utils.find(args, "agent") or frappe.get_doc("Agent", name)
     api = frappe.get_doc("API", agent.get("api"))
     token = api.get_password("token") or None
+    alias = agent.get("alias")
+    # Check if alias starts with '@', if not, add it
+    if alias and not alias.startswith("@"):
+        alias = "@" + alias
+    
     client = TelegramBot(token=token)
-    response = client.request(endpoint="/getChat", params={"chat_id": agent.get("alias")})
-    profile = response.json().get("data")
+
+    params = {"chat_id": alias}
+
+    response = client.request(
+        method="GET",
+        endpoint="/getChat",
+        params={"chat_id": alias}
+    )
+
+    profile = response.json().get("result")
+    
     display_name = profile.get("title") if profile.get("type") in ["group", "supergroup", "channel"] else f"{profile.get('first_name')} {profile.get('last_name')}" if profile.get("first_name") and profile.get("last_name") else profile.get("first_name") if profile.get("first_name") else profile.get("last_name") if profile.get("last_name") else profile.get("username")
-    picture_file = profile.get("photo").get("big_file_id") if profile.get("photo") else None
+    picture_id = profile.get("photo").get("big_file_id") if profile.get("photo") else None
     picture_url = None
-    if picture_file:
-        picture = client.request(endpoint="/getFile", params={"file_id": picture_file})
-        picture_url = client.request(method="GET", url=picture.json().get("data").get("file_path"), request=False)
-    frappe.get_doc("Agent", name).update({"uid": profile.get("id"), "display_name": display_name, "alias": profile.get("username"), "picture": picture_url}).save()
-    frappe.db.commit()
+    if picture_id:
+        picture = client.request(endpoint="/getFile", params={"file_id": picture_id})
+        picture_url = client.request(method="GET", url=picture.json().get("result").get("file_path"), request=False)
+    if profile.get("id") and display_name:
+        frappe.get_doc("Agent", name).update({"uid": profile.get("id"), "display_name": display_name, "alias": profile.get("username"), "picture": picture_url}).save()
+        frappe.db.commit()
 
 
 @frappe.whitelist()
@@ -57,11 +72,12 @@ def send(**args):
     # Check if alias starts with '@', if not, add it
     if alias and not alias.startswith("@"):
         alias = "@" + alias
+    chat_id = agent.get("uid") or alias
     linked_external_id = utils.find(args, "linked_external_id")
 
     client = TelegramBot(token=token)
 
-    params = {"chat_id": alias, "text": text}
+    params = {"chat_id": chat_id, "text": text}
 
     if linked_external_id:
         params.update({"reply_to_message_id": linked_external_id})
