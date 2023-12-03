d14e5607751f3a82c28c73fc315ba552f6a1fe6e
MIMIZA <dev@mimiza.com>
2023-12-03 19:39:17 +0800

    add length to content

--- smm/smm/doctype/api/api.js
+++ smm/smm/doctype/api/api.js
@@ -2,37 +2,37 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("API", {
-  refresh: (form) => {
-    website = frappe.urllib.get_base_url();
+    refresh: (form) => {
+        website = frappe.urllib.get_base_url();
 
-    fields = {
-      website: website,
-      //   redirect_uri: website + "/api/method/smm.libs.x.callback",
-      terms_of_services: website + "/terms-of-services",
-      privacy_policy: website + "/privacy-policy",
-    };
+        fields = {
+            website: website,
+            //   redirect_uri: website + "/api/method/smm.libs.x.callback",
+            terms_of_services: website + "/terms-of-services",
+            privacy_policy: website + "/privacy-policy",
+        };
 
-    modules = {
-      'X': 'x',
-      'Facebook': 'facebook'
-    }
+        modules = {
+            'X': 'x',
+            'Facebook': 'facebook'
+        }
 
-    const toggleFields = (item) => {
-      provider = item ? item?.target?.value : form.doc.provider;
-      visibility = ['X', 'Facebook'].includes(provider) ? true : false;
-      fields['redirect_uri'] = ['X', 'Facebook'].includes(provider) ? `${website}/api/method/smm.libs.${modules[provider]}.callback` : null
-      for (let field in fields) {
-        // Toggle field visibility
-        form.fields_dict[field].$wrapper.toggle(visibility);
-        // Set field data
-        form.doc[field] = fields[field];
-        // Rerender field
-        form.fields_dict[field].refresh();
-      }
-    };
+        const toggleFields = (item) => {
+            provider = item ? item?.target?.value : form.doc.provider;
+            visibility = ['X', 'Facebook'].includes(provider) ? true : false;
+            fields['redirect_uri'] = ['X', 'Facebook'].includes(provider) ? `${website}/api/method/smm.libs.${modules[provider]}.callback` : null
+            for (let field in fields) {
+                // Toggle field visibility
+                form.fields_dict[field].$wrapper.toggle(visibility);
+                // Set field data
+                form.doc[field] = fields[field];
+                // Rerender field
+                form.fields_dict[field].refresh();
+            }
+        };
 
-    toggleFields();
+        toggleFields();
 
-    form.fields_dict["provider"].$input.on("change", toggleFields);
-  },
+        form.fields_dict["provider"].$input.on("change", toggleFields);
+    },
 });
--- smm/smm/doctype/content/content.js
+++ smm/smm/doctype/content/content.js
@@ -1,8 +1,18 @@
 // Copyright (c) 2023, MIMIZA and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Content", {
-// 	refresh(frm) {
+frappe.ui.form.on("Content", {
+	refresh: (form) => {
+        const length = (item) => {
+            description = item ? item?.target?.value : form.doc.description;
+            // Set field data
+            form.doc["length"] = description.length;
+            // Rerender field
+            form.fields_dict["length"].refresh();
+        };
 
-// 	},
-// });
+        length();
+
+        form.fields_dict["description"].$input.on("change", length);
+	},
+});
