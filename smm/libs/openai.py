f4b846467957c1a4812145700c238677c3ea50c8
MIMIZA <dev@mimiza.com>
2023-12-20 20:10:26 +0800

    fixing...

--- smm/libs/openai.py
+++ smm/libs/openai.py
@@ -3,6 +3,7 @@ from frappe import _
 import random
 import json
 import requests
+import base64
 from ..libs import utils
 
 
@@ -11,19 +12,25 @@ class OpenAI:
         self.base_url = "https://api.openai.com"
         self.token = token
 
-    def headers(self):
+    def headers(self, headers={}):
         return {
             "Authorization": f"Bearer {self.token}",
             "Content-Type": "application/json",
+            **headers
         }
 
-    def request(self, method="POST", url=None, endpoint=None, data={}):
+    def request(self, method="POST", url=None, endpoint=None, data={}, headers={}, **args):
         url = url or self.base_url + endpoint
+        
+        if headers.get("Content-Type") == "application/json":
+            data =json.dumps(data)
+
         response = requests.request(
             method,
             url,
-            headers=self.headers(),
-            data=json.dumps(data)
+            headers=self.headers(headers),
+            data=data,
+            **args
         )
         return response
 
@@ -42,7 +49,7 @@ def generate_content(**args):
     if not frappe.db.exists("Content Mechanism", mechanism):
         frappe.msgprint(_("{0} {1} does not exist").format(_("Content Mechanism"), mechanism))
         return
-
+    
     doc = frappe.get_doc("Content Mechanism", mechanism)
     
     owner = doc.owner or frappe.get_user().name
@@ -50,7 +57,7 @@ def generate_content(**args):
     if doc.enabled == 0:
         frappe.msgprint(_("{0} {1} is disabled").format(_("Content Mechanism"), mechanism))
         return
-
+    
     feeds = []
     prompts = []
 
@@ -65,13 +72,14 @@ def generate_content(**args):
                 feeds.append(content)
 
     length = doc.length
-
+    generate_text = doc.generate_text
+    generate_image = doc.generate_image
+    generate_image_variation = doc.generate_image_variation
+    images = doc.images
     feed_provider_list = doc.feed_providers
-
     feed_list = doc.feeds
-
     prompt_list = doc.prompts
-
+    
     for item in feed_provider_list:
         feed_provider = frappe.get_doc("Feed Provider", item.feed_provider)
         # If feed provider is virtual, try to get feeds from the `feeds` field, which is a JSON array, then append to `feeds` list.
@@ -89,10 +97,10 @@ def generate_content(**args):
 
     for item in prompt_list:
         doc = frappe.get_doc("Prompt", item.prompt)
-        prompts.append({"role": "user", "content": doc.description})
+        prompts.append(doc.description)
 
     if len(feeds) > 0:
-        prompts.append({"role": "user", "content": json.dumps({"DATA": feeds})})
+        prompts.append(json.dumps({"DATA": feeds}))
 
     # Temporarily get random API. This needs to be fixed.
     apis = frappe.db.get_list("API", filters={"provider": "OpenAI"})
@@ -105,73 +113,183 @@ def generate_content(**args):
         return
 
     client = OpenAI(token)
-
-    data = {
-        "model": "gpt-3.5-turbo",
-        # "model": "gpt-4-turbo",
-        "messages": prompts,
-        "n": 1,
-        "tools": [
-            {
-                "type": "function",
-                "function": {
-                    "name": "generate_content",
-                    "description": "Create a content from given prompts and DATA. Returns `title` and `description`.",
-                    "parameters": {
-                        "type": "object",
-                        "properties": {
-                            "title": {
-                                "type": "string",
-                                "description": "The shortest version of the content."
+    
+    new_doc = frappe.get_doc({
+        "owner": owner,
+        "doctype": "Content",
+        "mechanism": mechanism
+    })
+    
+    if generate_text:
+        messages = []
+        for prompt in prompts:
+            messages.append({"role": "user", "content": prompt})
+        data = {
+            "model": "gpt-3.5-turbo",
+            # "model": "gpt-4-turbo",
+            "messages": messages,
+            "n": 1,
+            "tools": [
+                {
+                    "type": "function",
+                    "function": {
+                        "name": "generate_content",
+                        "description": "Create a content from given prompts and DATA. Returns `title` and `description`.",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {
+                                "title": {
+                                    "type": "string",
+                                    "description": "The shortest version of the content."
+                                },
+                                "description": {
+                                    "type": "string",
+                                    "description": "Content Description." + f" Maximum number of characters is {length}." if length and length > 0 else ""
+                                },
                             },
-                            "description": {
-                                "type": "string",
-                                "description": "Content Description." + f" Maximum number of characters is {length}." if length and length > 0 else ""
-                            },
-                        },
-                        "required": ["title", "description"]
+                            "required": ["title", "description"]
+                        }
                     }
                 }
-            }
-        ],
-        "tool_choice": {
-            "type": "function",
-            "function": {
-                "name": "generate_content"
+            ],
+            "tool_choice": {
+                "type": "function",
+                "function": {
+                    "name": "generate_content"
+                }
             }
         }
-    }
+    
+        response = client.request(endpoint="/v1/chat/completions", data=data)
+        data = response.json()
+        error = data.get("error") or {}
+        message = error.get("message") or {}
+    
+        if response.status_code != 200 and message:
+            frappe.msgprint(_(message))
+    
+        if response.status_code == 200:
+            choice = random.choice(data.get("choices"))
+            message = choice.get("message").get("tool_calls")[0].get("function").get("arguments")
+            message = json.loads(message)
+            title = message.get("title")
+            title = utils.remove_mentions(title)
+            title = utils.remove_quotes(title)
+            description = message.get("description")
+            description = utils.remove_mentions(description)
+            description = utils.remove_quotes(description)
+            new_doc.update({
+                "title": title,
+                "description": description if len(description) > len(title) else title
+            })
+            new_doc.insert()
+            frappe.db.commit()
+    
+    if generate_image:
+        data = {
+            "model": "dall-e-2",
+            "prompt": ". ".join(prompts) if len(prompts) > 0 else "",
+            "response_format": "b64_json", # Must be "url" or "b64_json"
+            "size": "256x256", # 256x256, 512x512, 1024x1024
+            "quality": "standard",
+            "style": "natural", # Must be "natural" or "vivid"
+            "n": 1
+        }
+        response = client.request(endpoint="/v1/images/generations", data=data)
+        data = response.json()
+        error = data.get("error") or {}
+        message = error.get("message") or {}
+        
+        if response.status_code != 200 and message:
+            frappe.msgprint(_(message))
+    
+        if response.status_code == 200:
+            data = random.choice(data.get("data"))
+            content = data.get("b64_json")
+            random_name = frappe.utils.random_string(24) + ".png"
+            if not new_doc.name:
+                new_doc.insert()
+                frappe.db.commit()
+            check_folder(name="SMM")
+            file = frappe.utils.file_manager.save_file(random_name, content, dt="Content", dn=new_doc.name, df="image", folder="Home/SMM", decode=True, is_private=False)
+            new_doc.update({
+                "image": file.get("file_url")
+            })
+            new_doc.save()
+            frappe.db.commit()
+            
+    
+    if generate_image_variation:
+        image = frappe.get_doc("File", random.choices(images)[0].image)
+        file_path = frappe.utils.file_manager.get_file_path(image.file_name)
+        # print("PATH", file_path)
+        file = frappe.utils.file_manager.get_file(image.file_name)
+        file_name = file[0]
+        file_content = file[1]
+        
+        # print("FILE", file)
+        # files = {'image': (file_path, file, 'image/png')}
+        # files = {'image': (file_path, file)}
+        # convert to base64 string
 
-    response = client.request(endpoint="/v1/chat/completions", data=data)
-    data = response.json()
-    error = data.get("error") or {}
-    message = error.get("message") or {}
+        # file_content = base64.b64encode(file.read())
+        
 
-    if response.status_code != 200 and message:
-        frappe.msgprint(_(message))
-        return response
+        # Create the JSON payload with the 'image' property
+        payload = {'image': file_content}
+            
+        data = {
+            "model": "dall-e-2",
+            "image": file,
+            "response_format": "b64_json", # Must be "url" or "b64_json"
+            "size": "256x256", # 256x256, 512x512, 1024x1024
+            "n": 1
+        }
 
-    if response.status_code == 200:
-        choice = random.choice(data.get("choices"))
-        message = choice.get("message").get("tool_calls")[0].get("function").get("arguments")
-        message = json.loads(message)
-        title = message.get("title")
-        title = utils.remove_mentions(title)
-        title = utils.remove_quotes(title)
-        description = message.get("description")
-        description = utils.remove_mentions(description)
-        description = utils.remove_quotes(description)
-        doc = frappe.get_doc({
-            "owner": owner,
-            "doctype": "Content",
-            "mechanism": mechanism,
-            "title": title,
-            "description": description if len(description) > len(title) else title,
+        response = client.request(endpoint="/v1/images/variations", headers={"Content-Type": "multipart/form-data"}, data=data, json=payload)
+        print(response.status_code, response.content)
+        return
+
+        data = response.json()
+        error = data.get("error") or {}
+        message = error.get("message") or {}
+
+        print("DATA", data)
+        
+        if response.status_code != 200 and message:
+            frappe.msgprint(_(message))
+        
+        if response.status_code == 200:
+            data = random.choice(data.get("data"))
+            content = data.get("b64_json")
+            random_name = frappe.utils.random_string(24) + ".png"
+            if not new_doc.name:
+                new_doc.insert()
+                frappe.db.commit()
+            check_folder(name="SMM")
+            file = frappe.utils.file_manager.save_file(random_name, content, dt="Content", dn=new_doc.name, df="image", folder="Home/SMM", decode=True, is_private=False)
+            new_doc.update({
+                "image": file.get("file_url")
+            })
+            new_doc.save()
+            frappe.db.commit()
+        
+    
+    return True
+
+
+def check_folder(**args):
+    name = utils.find(args, "name") or ""
+    if not frappe.db.exists({"doctype": "File", "name": f"Home/{name}", "file_name": name, "is_folder": True}):
+        folder = frappe.get_doc({
+            "doctype": "File",
+            "file_name": name,
+            "is_folder": True,
+            "folder": "Home"
         })
-        doc.insert()
+        folder.insert()
         frappe.db.commit()
-        return doc
-
+        return folder
 
 # @frappe.whitelist()
 # def generate_image_variations(**args):
--- smm/smm/doctype/content/content.py
+++ smm/smm/doctype/content/content.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2023, MIMIZA and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from ....libs import utils
 from frappe.model.document import Document
 
@@ -11,6 +11,9 @@ class Content(Document):
         self.update_title()
 
     def update_title(self):
-        title = self.title or self.description or self.name
+        title = self.title or self.description
+        if self.mechanism:
+            mechanism = frappe.get_doc("Content Mechanism", self.mechanism)
+            title = mechanism.get("title") + " " + frappe.utils.random_string(24)
         title = utils.shorten_string(title)
         self.title = title
