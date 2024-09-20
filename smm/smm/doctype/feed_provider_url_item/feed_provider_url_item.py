36400551e59e02ad5705e14594cccbf2cc673ef7
Bang Vo Thanh <bang.vothanh@papagroup.net>
2024-09-20 15:36:04 +0700

    *update feed provider from single URL to multi URLs

---
+++ smm/smm/doctype/feed_provider_url_item/feed_provider_url_item.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, mimiza and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class FeedProviderURLItem(Document):
+	pass
